cb9c3f268afc4e33b822c446fd5a0f8f62c15b39
Wyatt Beavers <hammerfrenzy@gmail.com>
2021-10-02 02:51:13 -0500

    WRANGLE

--- Assets/Scripts/Cowpoke.cs
+++ Assets/Scripts/Cowpoke.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Cowpoke : MonoBehaviour
 {
+    private Animator _animator;
     private CharacterController _controller;
     private SpriteRenderer _renderer;
 
     [SerializeField]
     private InputActionReference _moveActionReference;
+    private InputAction _moveAction;
+
+    [SerializeField]
+    private InputActionReference _wrangleActionReference;
+    private InputAction _wrangleAction;
 
     private float _speed = 6f;
     private float _moveThreshold = 0.2f;
@@ -19,24 +27,42 @@ public class Cowpoke : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _animator = GetComponent<Animator>();
         _controller = GetComponent<CharacterController>();
         _renderer = GetComponent<SpriteRenderer>();
+
+        _moveAction = _moveActionReference.action;
+        _wrangleAction = _wrangleActionReference.action;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var direction = _moveActionReference.action.ReadValue<Vector2>();
+        var shouldWrangle = _wrangleAction.triggered;
+        if (shouldWrangle)
+        {
+            _animator.SetBool("IsWrangling", true);
+        }
+
+        var direction = _moveAction.ReadValue<Vector2>();
         DoMovement(direction);
     }
 
     public void DoMovement(Vector2 direction)
     {
-        Debug.Log($"got {direction} from input stuff");
-        if (direction.magnitude < _moveThreshold) { return; }
+        if (direction.magnitude < _moveThreshold)
+        {
+            _animator.SetBool("IsMoving", false);
+            return;
+        }
 
         _renderer.flipX = direction.x < 0;
-
         _controller.Move(direction * _speed * Time.deltaTime);
+        _animator.SetBool("IsMoving", true);
+    }
+
+    private void DoneWranglin()
+    {
+        _animator.SetBool("IsWrangling", false);
     }
 }
